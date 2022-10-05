84bb180887ba230f94dadef18a9cd2d18d2132fc
tjzhang1 <tylerjzhang@gmail.com>
2022-10-05 15:14:51 -0700

    revise header for TMC for a simple module

--- smartHighway/src/TrafficManagementCenter/TMC.h
+++ smartHighway/src/TrafficManagementCenter/TMC.h
@@ -5,10 +5,22 @@
 
 namespace veins {
 
-class TMC : public DemoBaseApplLayer {
+class TMC : public cSimpleModule {
 protected:
-    void onWSM(BaseFrame1609_4* wsm) override;
-    void onWSA(DemoServiceAdvertisment* wsa) override;
+    void initialize(int stage) override;
+    void handleMessage(cMessage *msg) override;
+    void finish(void) override;
+
+    // Determine the control output for the system (signal timings, broadcast to reroute)
+    void computeAction(void);
+    // Control: update signal timing for a particular RSU
+    void updateSignalTiming(void);
+    // Control: tell RSU to broadcast vehicles to reroute
+    void broadcastReroute(void);
+    // Data: sample the availability of registered park n rides
+    void parkingLotStatus(void);
+    // Data: sample the position and velocities of cars near RSUs
+    void collectTrafficInfo(void);
 };
 
 }

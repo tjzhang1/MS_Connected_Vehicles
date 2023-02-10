e27eff621852cc803b7bd37c43e0359550b8998e
tjzhang1 <tylerjzhang@gmail.com>
2023-02-10 12:28:30 -0800

    added optimization for continuing vehicles

--- smartHighway/src/CarApplication/ContinuingCarApp.cc
+++ smartHighway/src/CarApplication/ContinuingCarApp.cc
@@ -13,33 +13,33 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
-#include <CarApplication/BusApp.h>
+#include <CarApplication/ContinuingCarApp.h>
 
 using namespace veins;
 
-Define_Module(veins::BusApp);
+Define_Module(veins::ContinuingCarApp);
 
-BusApp::BusApp() {
+ContinuingCarApp::ContinuingCarApp() {
     // TODO Auto-generated constructor stub
 }
 
-BusApp::~BusApp() {
+ContinuingCarApp::~ContinuingCarApp() {
     // TODO Auto-generated destructor stub
 }
 
-void BusApp::initialize(int stage) {
+void ContinuingCarApp::initialize(int stage) {
     BasicCarApp::initialize(stage);
     if(stage == 0) {
         spawnTime = simTime();
         TMC_connection = dynamic_cast<TMC *>(getParentModule()->getParentModule()->getSubmodule("TMC"));
-        // Collect contents of HOV reward buffer
-        payloadReward = TMC_connection->bufferedHOVReward;
-        // Reset HOV reward buffer
-        TMC_connection->bufferedHOVReward = {0,SimTime::ZERO,0};
+        // Collect contents of continuing vehicle reward buffer
+        payloadReward = TMC_connection->bufferedVehReward;
+        // Reset continuing vehicle reward buffer
+        TMC_connection->bufferedVehReward = {0,SimTime::ZERO,0};
     }
 }
 
-void BusApp::finish() {
+void ContinuingCarApp::finish() {
     BasicCarApp::finish();
     TMC_connection->globalReward.hwyThroughput++;
     TMC_connection->globalReward.accumTravelTime+=(simTime() - spawnTime);
@@ -48,15 +48,15 @@ void BusApp::finish() {
     TMC_connection->globalReward.hwyThroughput += payloadReward.hwyThroughput;
     TMC_connection->globalReward.accumTravelTime += payloadReward.accumTravelTime;
     TMC_connection->globalReward.accumCO2Emissions += payloadReward.accumCO2Emissions;
-#if BUSAPP_VERBOSE
-    std::cout<<"Exited a bus with travel time " << (simTime()-spawnTime).str() << "s and emissions " << CO2Emissions <<"\t";
+#if CONTCARAPP_VERBOSE
+    std::cout<<"Exited a continuing car with travel time " << (simTime()-spawnTime+payloadReward.accumTravelTime).str() << "s and emissions " << CO2Emissions+payloadReward.accumCO2Emissions<<"\t";
     std::cout<<"THROUGHPUT: "<<TMC_connection->globalReward.hwyThroughput<<", "<<"TIME: "<<TMC_connection->globalReward.accumTravelTime.str()<<", "<<"Emissions: "<<TMC_connection->globalReward.accumCO2Emissions<<endl;
 #endif
 }
 
-void BusApp::handlePositionUpdate(cObject* obj) {
+void ContinuingCarApp::handlePositionUpdate(cObject* obj) {
     CO2Emissions += traciVehicle->getCO2Emissions();
-#if BUSAPP_VERY_VERBOSE
+#if CONTCARAPP_VERY_VERBOSE
     std::cout<<getParentModule()->getFullName()<<" co2: "<<CO2Emissions<<" mg"<<endl;
 #endif
 }
